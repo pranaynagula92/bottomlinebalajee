a2d4c7218cc9ab9027d39fe6349b38c014d2ccfe
pranaynagula92 <62459863+pranaynagula92@users.noreply.github.com>
2024-03-14 00:39:25 -0700

    UI

--- js/main.js
+++ js/main.js
@@ -103,30 +103,30 @@
 	
 	};
 
-	var pieChart = function() {
-		$('.chart').easyPieChart({
-			scaleColor: false,
-			lineWidth: 4,
-			lineCap: 'butt',
-			barColor: '#FF9000',
-			trackColor:	"#f5f5f5",
-			size: 160,
-			animate: 1000
-		});
-	};
+	// var pieChart = function() {
+	// 	$('.chart').easyPieChart({
+	// 		scaleColor: false,
+	// 		lineWidth: 4,
+	// 		lineCap: 'butt',
+	// 		barColor: '#FF9000',
+	// 		trackColor:	"#f5f5f5",
+	// 		size: 160,
+	// 		animate: 1000
+	// 	});
+	// };
 
-	var skillsWayPoint = function() {
-		if ($('#fh5co-skills').length > 0 ) {
-			$('#fh5co-skills').waypoint( function( direction ) {
+	// var skillsWayPoint = function() {
+	// 	if ($('#fh5co-skills').length > 0 ) {
+	// 		$('#fh5co-skills').waypoint( function( direction ) {
 										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
+	// 			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+	// 				setTimeout( pieChart , 400);					
+	// 				$(this.element).addClass('animated');
+	// 			}
+	// 		} , { offset: '90%' } );
+	// 	}
 
-	};
+	// };
 
 
 	// Loading page
@@ -142,7 +142,7 @@
 		fullHeight();
 		parallax();
 		// pieChart();
-		skillsWayPoint();
+		// skillsWayPoint();
 	});
 
 
